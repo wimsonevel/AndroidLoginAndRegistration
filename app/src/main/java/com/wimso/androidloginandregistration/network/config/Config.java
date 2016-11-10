65f1daeaae85b346ce9d9682fb7274c107b138cb
Wim Sonevel <wim.sonevel@yahoo.co.id>
2016-11-10 19:17:01 +0700

    config

--- app/src/main/java/com/wimso/androidloginandregistration/network/config/Config.java
+++ app/src/main/java/com/wimso/androidloginandregistration/network/config/Config.java
@@ -5,7 +5,6 @@ package com.wimso.androidloginandregistration.network.config;
  */
 public class Config {
 
-//    public static final String BASE_URL = "http://elainehartanto.com";
     public static final String BASE_URL = "http://192.168.2.31"; // Your Local IP Address
 
     public static final String API_URL = BASE_URL + "/simple_api";
